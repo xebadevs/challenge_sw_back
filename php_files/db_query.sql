b3054b98d9095dfca262f03e10c4224a4e355313
Sebastian Londero <xebadevs@gmail.com>
2022-04-17 22:28:28 -0300

    db_query

--- php_files/db_query.sql
+++ php_files/db_query.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 18-04-2022 a las 02:58:44
+-- Tiempo de generación: 18-04-2022 a las 03:28:15
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
@@ -33,8 +33,16 @@ CREATE TABLE `attributes` (
   `weight` float DEFAULT NULL,
   `height` float DEFAULT NULL,
   `width` float DEFAULT NULL,
-  `length` float DEFAULT NULL
+  `length` float DEFAULT NULL,
+  `fk_id_product` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `attributes`
+--
+
+INSERT INTO `attributes` (`id_attribute`, `size`, `weight`, `height`, `width`, `length`, `fk_id_product`) VALUES
+(2, 700, NULL, NULL, NULL, NULL, 5);
 
 -- --------------------------------------------------------
 
@@ -46,9 +54,15 @@ CREATE TABLE `products` (
   `id_prod` int(11) NOT NULL,
   `sku` varchar(20) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `price` float NOT NULL,
-  `fk_attribute` int(11) NOT NULL
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id_prod`, `sku`, `name`, `price`) VALUES
+(5, 'JVC200123', 'Acme DISC', 1);
 
 --
 -- Índices para tablas volcadas
@@ -58,14 +72,14 @@ CREATE TABLE `products` (
 -- Indices de la tabla `attributes`
 --
 ALTER TABLE `attributes`
-  ADD PRIMARY KEY (`id_attribute`);
+  ADD PRIMARY KEY (`id_attribute`),
+  ADD KEY `fk_id_product` (`fk_id_product`);
 
 --
 -- Indices de la tabla `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id_prod`),
-  ADD KEY `fk_attribute` (`fk_attribute`);
+  ADD PRIMARY KEY (`id_prod`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -75,13 +89,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `attributes`
 --
 ALTER TABLE `attributes`
-  MODIFY `id_attribute` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_attribute` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
@@ -91,13 +105,7 @@ ALTER TABLE `products`
 -- Filtros para la tabla `attributes`
 --
 ALTER TABLE `attributes`
-  ADD CONSTRAINT `attributes_ibfk_1` FOREIGN KEY (`id_attribute`) REFERENCES `products` (`fk_attribute`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`fk_attribute`) REFERENCES `attributes` (`id_attribute`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `attributes_ibfk_1` FOREIGN KEY (`fk_id_product`) REFERENCES `products` (`id_prod`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
